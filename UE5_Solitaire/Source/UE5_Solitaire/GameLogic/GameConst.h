9a9360ca55d1d49446ef4fa38004394da3d84c25
xuke <825126369@qq.com>
2025-10-21 10:36:54 +0800

    2

---
+++ UE5_Solitaire/Source/UE5_Solitaire/GameLogic/GameConst.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+static class UE5_SOLITAIRE_API GameConst
+{
+private:
+    // 1. 私有构造函数：禁止实例化
+    GameConst() = delete;
+    // 2. 禁止拷贝
+    GameConst(const GameConst&) = delete;
+    GameConst& operator=(const GameConst&) = delete;
+public:
+    static int EventId_InitSceneHotUpdate_1 = 0;
+};
