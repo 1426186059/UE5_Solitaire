9ce76d5b41415204648df808e4eb3d4a7d1cbe00
xuke <825126369@qq.com>
2025-10-31 09:28:45 +0800

    2

--- UE5_Solitaire/Source/UE5_Solitaire/GameLogic/ResCenter.h
+++ UE5_Solitaire/Source/UE5_Solitaire/GameLogic/ResCenter.h
@@ -9,9 +9,12 @@
 #include "CardHandler.h"
 #include "DataCenter.h"
 #include "ThemeDataHandler.h"
+#include "ResCenter.generated.h"
 
+UCLASS()
 class AResCenter : public AKKActorSingleton
 {
+	GENERATED_BODY()
 public:
 	static AResCenter* GetSingleton(bool bCreate = true)
 	{
