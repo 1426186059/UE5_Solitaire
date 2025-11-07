704b392590fc82f43fc81305add2fc92f33999f9
xuke <825126369@qq.com>
2025-11-07 12:07:12 +0800

    2

--- UE5_Solitaire/Source/UE5_Solitaire/DataTable/DTMgr.cpp
+++ UE5_Solitaire/Source/UE5_Solitaire/DataTable/DTMgr.cpp
@@ -6,6 +6,6 @@ TMap<TSubclassOf<UDataTable>, TWeakObjectPtr<UDataTable>> DTMgr::mConfigDic = {}
 
 void DTMgr::Init()
 {
-   // this->LoadTable<UDataTable_jianhuan_vita>(TEXT("/Game/ResourceABs/MainScene/Config/DT/jianhuan_vita.jianhuan_vita"));
-   // this->LoadTable<UDataTable_i18n>(TEXT("/Game/ResourceABs/MainScene/Config/DT/i18n.i18n"));
+    this->LoadTable<UDataTable_jianhuan_vita>(TEXT("/Game/ResourceABs/MainScene/Config/DT/jianhuan_vita.jianhuan_vita"));
+    this->LoadTable<UDataTable_i18n>(TEXT("/Game/ResourceABs/MainScene/Config/DT/i18n.i18n"));
 }
