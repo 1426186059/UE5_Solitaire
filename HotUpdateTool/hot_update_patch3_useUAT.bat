7342d3c14e49b6541b882fe28364890bf8d58721
xuke <825126369@qq.com>
2025-11-21 00:05:04 +0800

    UAT 自动化 失败

---
+++ HotUpdateTool/hot_update_patch3_useUAT.bat
@@ -0,0 +1,10 @@
+RunUAT.bat ^
+BuildCookRun ^
+-project="D:\Me\MyProject\UE5_Solitaire\UE5_Solitaire\UE5_Solitaire.uproject" ^
+-platform=ANDROID ^
+-cookflavor=ASTC ^
+-clientconfig=Shipping ^
+-build -cook -stage -archive ^
+-archivedirectory="D:\Me\MyProject\UE5_Solitaire\HotUpdatePak_UAT\" ^
+-CookDir="D:\Me\MyProject\UE5_Solitaire\UE5_Solitaire\Saved\Cooked\" ^
+-pak -compressed -utf8output
