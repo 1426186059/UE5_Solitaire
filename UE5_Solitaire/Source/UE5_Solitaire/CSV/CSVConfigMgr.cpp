1ff18c5c7f57f4ff5d3723c867b1b68025f6092f
xuke <825126369@qq.com>
2025-11-21 10:45:48 +0800

    只用 UDataTable

--- UE5_Solitaire/Source/UE5_Solitaire/CSV/CSVConfigMgr.cpp
+++ UE5_Solitaire/Source/UE5_Solitaire/CSV/CSVConfigMgr.cpp
@@ -4,11 +4,11 @@
 
 void CSVConfigMgr::Init()
 {
-    this->LoadCSV<csv_i18n>("i18n.csv");
-    this->LoadCSV<csv_jianhuan_vita>("jianhuan_vita.csv");
-    this->LoadCSV<csv_jianhuan_newbie>("jianhuan_newbie.csv");
-    this->LoadCSV<csv_stagereward>("1105stagereward.csv");
-    this->LoadCSV<csv_table2element>("1301table2element.csv");
-    this->LoadCSV<csv_theme>("1203theme.csv");
-    this->LoadCSV<csv_themeitem>("1104themeitem.csv");
+    //this->LoadCSV<csv_i18n>("i18n.csv");
+    //this->LoadCSV<csv_jianhuan_vita>("jianhuan_vita.csv");
+    //this->LoadCSV<csv_jianhuan_newbie>("jianhuan_newbie.csv");
+    //this->LoadCSV<csv_stagereward>("1105stagereward.csv");
+    //this->LoadCSV<csv_table2element>("1301table2element.csv");
+    //this->LoadCSV<csv_theme>("1203theme.csv");
+    //this->LoadCSV<csv_themeitem>("1104themeitem.csv");
 }
