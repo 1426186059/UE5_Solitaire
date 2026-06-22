0761a7785b26f85b7e5a86687f6aae95d8072b7b
xuke2026 <825126369@qq.com>
2026-06-22 23:30:10 +0800

    o

--- UE5_Solitaire/Source/UE5_Solitaire.Target.cs
+++ UE5_Solitaire/Source/UE5_Solitaire.Target.cs
@@ -8,8 +8,8 @@ public class UE5_SolitaireTarget : TargetRules
 	public UE5_SolitaireTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 
 		ExtraModuleNames.AddRange(new string[] { 
 			"UE5_Solitaire",
